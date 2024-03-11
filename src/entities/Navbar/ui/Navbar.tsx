a882d087324865c7924d59fd5b58bda5b583d67a
Nruzhzhaugash <clonn262@gmail.com>
2024-03-12 01:52:37 +0500

    delete some code

--- src/entities/Navbar/ui/Navbar.tsx
+++ src/entities/Navbar/ui/Navbar.tsx
@@ -1,49 +1,39 @@
 import React from "react";
 import Link from "next/link";
-// import styles from "./styles.module.scss";
 
 export default function Navbar() {
   return (
-<<<<<<< HEAD
-    <nav className='flex items-center gap-11'>
-      <Link href='' className="whitespace-nowrap font-obrazec uppercase font-extrabold text-link text-[32px]">О клубе</Link>
-      <Link href='' className="whitespace-nowrap font-obrazec uppercase font-extrabold text-link text-[32px]">Расписание</Link>
-      <Link href='' className="whitespace-nowrap font-obrazec uppercase font-extrabold text-link text-[32px]">Каталог</Link>
-      <Link href='' className="whitespace-nowrap font-obrazec uppercase font-extrabold text-link text-[32px]">Рейтинг</Link>
-      <Link href='' className="whitespace-nowrap font-obrazec uppercase font-extrabold text-link text-[32px]">Магазин</Link>
-=======
     <nav className="flex items-center gap-11">
       <Link
         href=""
-        className="whitespace-nowrap font-obrazec uppercase font-extrabold text-link text-lg"
+        className="whitespace-nowrap font-obrazec uppercase font-extrabold text-link text-[32px]"
       >
         О клубе
       </Link>
       <Link
         href=""
-        className="whitespace-nowrap font-obrazec uppercase font-extrabold text-link text-base"
+        className="whitespace-nowrap font-obrazec uppercase font-extrabold text-link text-[32px]"
       >
         Расписание
       </Link>
       <Link
         href=""
-        className="whitespace-nowrap font-obrazec uppercase font-extrabold text-link text-base"
+        className="whitespace-nowrap font-obrazec uppercase font-extrabold text-link text-[32px]"
       >
         Новости
       </Link>
       <Link
         href=""
-        className="whitespace-nowrap font-obrazec uppercase font-extrabold text-link text-base"
+        className="whitespace-nowrap font-obrazec uppercase font-extrabold text-link text-[32px]"
       >
         Рейтинг
       </Link>
       <Link
         href=""
-        className="whitespace-nowrap font-obrazec uppercase font-extrabold text-link text-base"
+        className="whitespace-nowrap font-obrazec uppercase font-extrabold text-link text-[32px]"
       >
         Магазин
       </Link>
->>>>>>> feature-main
     </nav>
   );
 }
