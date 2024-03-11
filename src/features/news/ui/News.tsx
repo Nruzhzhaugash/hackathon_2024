dede3ceb8c7640eaea5ef6e1d577cab3aa36a9f3
zen_vov <zholamanbv@gmail.com>
2024-03-12 02:07:34 +0500

    main completed

--- src/features/news/ui/News.tsx
+++ src/features/news/ui/News.tsx
@@ -13,7 +13,9 @@ interface NewsFeedProps {
 const NewsFeed: React.FC<NewsFeedProps> = ({ news }) => {
   return (
     <div className="w-[665px] text-">
-      <h1 className="text-2xl text-primary mb-[20px]">Новости</h1>
+      <h1 className="text-2xl text-primary mb-[20px] font-keetanoKB">
+        Новости
+      </h1>
       <div className="flex flex-col items-center justify-center">
         {news.map((item) => (
           <div key={item.id} className="w-[665px] mb-[20px] overflow-hidden">
