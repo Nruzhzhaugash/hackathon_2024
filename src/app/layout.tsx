d3490e3ea08f6eb9e3def72ac17d5e55971cdbd8
zen_vov <zholamanbv@gmail.com>
2024-03-11 17:00:34 +0500

    relieve font added

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,17 +1,22 @@
 import type { Metadata } from "next";
-import localFont from 'next/font/local';
+import localFont from "next/font/local";
 import "./styles/globals.css";
 
 const myFont = localFont({
-  src: '../../public/fonts/keetano_katana.ttf',
-  display: 'swap',
-  variable: '--font-keetano'
-})
+  src: "../../public/fonts/keetano_katana.ttf",
+  display: "swap",
+  variable: "--font-keetano",
+});
 const obrazec = localFont({
-  src: '../../public/fonts/Obrazec-2.ttf',
-  display: 'swap',
-  variable: '--font-obrazec'
-})
+  src: "../../public/fonts/Obrazec-2.ttf",
+  display: "swap",
+  variable: "--font-obrazec",
+});
+const relieve = localFont({
+  src: "../../public/fonts/Relieve.ttf",
+  display: "swap",
+  variable: "--font-relieve",
+});
 
 export const metadata: Metadata = {
   title: "Mafia site",
