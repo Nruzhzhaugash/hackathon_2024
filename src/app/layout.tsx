9ded06530f46864559df6cae8c0a6d6a8fd1f733
Nruzhzhaugash <clonn262@gmail.com>
2024-03-11 16:39:17 +0500

    add font

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,8 +1,14 @@
 import type { Metadata } from "next";
-import { Inter } from "next/font/google";
+// import { Inter } from "next/font/google" ;
+import localFont from 'next/font/local';
 import "./styles/globals.css";
 
-const inter = Inter({ subsets: ["latin", "cyrillic"] });
+// const inter = Inter({ subsets: ["latin", "cyrillic"] });
+
+const myFont = localFont({
+  src: '../../public/fonts/keetano_katana.ttf',
+  display: 'swap'
+})
 
 export const metadata: Metadata = {
   title: "Mafia site",
@@ -16,7 +22,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={inter.className}>
+      <body className={myFont.className}>
         {children}
       </body>
     </html>
