01cea6e3483d68e02d4f1c6999b3030fe532066d
zen_vov <zholamanbv@gmail.com>
2024-04-11 17:08:17 +0500

    auth

--- src/app/rating/page.tsx
+++ src/app/rating/page.tsx
@@ -6,8 +6,12 @@ export default function Rating() {
     <Layout type="login">
       <main className="container pt-[60px] pb-[120px]">
         <RatingPage />
-        <p className="text-base mt-[50px] font-normal font-involveBQ text-center text-primary">В конце каждого месяца и года мы награждаем лучших игроков клуба ценными призами и званиями. Это отличная возможность для вас продемонстрировать свои навыки и стать частью истории нашего клуба.</p>
+        <p className="text-base mt-[50px] font-normal font-involveBQ text-center text-primary">
+          В конце каждого месяца и года мы награждаем лучших игроков клуба
+          ценными призами и званиями. Это отличная возможность для вас
+          продемонстрировать свои навыки и стать частью истории нашего клуба.
+        </p>
       </main>
     </Layout>
-  )
+  );
 }
